dff989b1bf0bac4227ad8ba5a8f9405da0fd2565
Jolann <jmadec@enseirb-matmeca.fr>
2024-11-05 15:38:34 +0100

    INIT

--- refactoring/olderProject.js
+++ refactoring/olderProject.js
@@ -1,39 +1,30 @@
-function getRandomArrayElement(array) {
-    return array[Math.floor(Math.random() * array.length)];
+function getRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function getRandomIntegerInRange(min, max) {
+function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const PLANET_NAMES = ["Aqua", "Zyron", "Thalor", "Nox", "Verdana", "Kromos", "Luna", "Solaris"];
-const BIOMES = ["désert", "forêt tropicale", "océan", "montagne", "toundra", "plaines", "volcanique"];
-const RESOURCES = ["minerai", "eau", "carburant", "bois", "plantes médicinales", "cristaux", "énergie solaire"];
+const planetNames = ["Aqua", "Zyron", "Thalor", "Nox", "Verdana", "Kromos", "Luna", "Solaris"];
+const biomes = ["désert", "forêt tropicale", "océan", "montagne", "toundra", "plaines", "volcanique"];
+const resources = ["minerai", "eau", "carburant", "bois", "plantes médicinales", "cristaux", "énergie solaire"];
 
 class Planet {
     constructor(name) {
-        this.name = name || getRandomArrayElement(PLANET_NAMES);
-        this.biome = this.assignRandomBiome();
-        this.population = this.generateRandomPopulation();
-        this.resources = this.generateUniqueResources();
+        this.name = name || getRandomElement(planetNames);
+        this.biome = getRandomElement(biomes);
+        this.population = getRandomInt(1000, 1000000);
+        this.resources = this.generateResources();
     }
 
-    assignRandomBiome() {
-        return getRandomArrayElement(BIOMES);
-    }
-
-    generateRandomPopulation() {
-        return getRandomIntegerInRange(1000, 1000000);
-    }
-
-    generateUniqueResources() {
-        const numberOfResources = getRandomIntegerInRange(1, 3);
-        const selectedResources = new Set();
-        
-        while (selectedResources.size < numberOfResources) {
-            selectedResources.add(getRandomArrayElement(RESOURCES));
+    generateResources() {
+        const numResources = getRandomInt(1, 3);
+        const availableResources = [];
+        for (let i = 0; i < numResources; i++) {
+            availableResources.push(getRandomElement(resources));
         }
-        return Array.from(selectedResources);
+        return [...new Set(availableResources)];
     }
 
     describe() {
@@ -45,15 +36,17 @@ class Planet {
 }
 
 class SolarSystem {
-    constructor(numberOfPlanets) {
-        this.planets = this.createPlanets(numberOfPlanets);
+    constructor(numPlanets) {
+        this.planets = this.generatePlanets(numPlanets);
     }
 
-    createPlanets(numberOfPlanets) {
-        return Array.from({ length: numberOfPlanets }, () => {
-            const planetName = `${getRandomArrayElement(PLANET_NAMES)}-${getRandomIntegerInRange(1, 100)}`;
-            return new Planet(planetName);
-        });
+    generatePlanets(numPlanets) {
+        const planets = [];
+        for (let i = 0; i < numPlanets; i++) {
+            const planetName = getRandomElement(planetNames) + "-" + getRandomInt(1, 100);
+            planets.push(new Planet(planetName));
+        }
+        return planets;
     }
 
     describe() {
@@ -63,16 +56,16 @@ class SolarSystem {
         });
     }
 
-    getMostPopulatedPlanet() {
-        return this.planets.reduce((mostPopulated, currentPlanet) => 
-            currentPlanet.population > mostPopulated.population ? currentPlanet : mostPopulated
+    findRichestPlanet() {
+        return this.planets.reduce((richest, planet) => 
+            planet.population > richest.population ? planet : richest
         );
     }
 }
 
-const generatedSolarSystem = new SolarSystem(5);
-generatedSolarSystem.describe();
+const mySolarSystem = new SolarSystem(5);
+mySolarSystem.describe();
 
 console.log("\nPlanète la plus peuplée :");
-const mostPopulatedPlanet = generatedSolarSystem.getMostPopulatedPlanet();
-console.log(mostPopulatedPlanet.describe());
+const richestPlanet = mySolarSystem.findRichestPlanet();
+console.log(richestPlanet.describe());
