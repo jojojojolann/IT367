0aef1df7419bab814911ba73a48781d233385315
Jolann <jmadec@enseirb-matmeca.fr>
2024-11-05 15:33:03 +0100

    Delete echauffement.js

--- refactoring/echauffement.js
+++
@@ -1,9 +0,0 @@
-function getRandomElement(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-}
-
-const a = getRandomElement(["foo", "bar"]);
-const b = getRandomElement([1, 2]);
-const c = getRandomElement([{ path: "/foo" }, { path: "/bar" }]);
-
-console.log(a, b, c);
