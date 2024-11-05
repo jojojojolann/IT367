67d8d12f36a4d91bcd02b53f04afc255f96889b9
Jolann <jmadec@enseirb-matmeca.fr>
2024-11-05 15:08:50 +0100

    beforeRefactoring

--- refactoring/olderProject.js
+++ refactoring/olderProject.js
@@ -1,4 +1,3 @@
-// Utilitaires pour la génération aléatoire
 function getRandomElement(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
@@ -7,28 +6,25 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Données pour générer des planètes
 const planetNames = ["Aqua", "Zyron", "Thalor", "Nox", "Verdana", "Kromos", "Luna", "Solaris"];
 const biomes = ["désert", "forêt tropicale", "océan", "montagne", "toundra", "plaines", "volcanique"];
 const resources = ["minerai", "eau", "carburant", "bois", "plantes médicinales", "cristaux", "énergie solaire"];
 
-// Classe pour une planète
 class Planet {
     constructor(name) {
         this.name = name || getRandomElement(planetNames);
         this.biome = getRandomElement(biomes);
-        this.population = getRandomInt(1000, 1000000); // Population entre 1,000 et 1,000,000
+        this.population = getRandomInt(1000, 1000000);
         this.resources = this.generateResources();
     }
 
     generateResources() {
-        // Génère une liste aléatoire de ressources disponibles
         const numResources = getRandomInt(1, 3);
         const availableResources = [];
         for (let i = 0; i < numResources; i++) {
             availableResources.push(getRandomElement(resources));
         }
-        return [...new Set(availableResources)]; // Supprime les doublons
+        return [...new Set(availableResources)];
     }
 
     describe() {
@@ -39,7 +35,6 @@ class Planet {
     }
 }
 
-// Classe pour le système solaire
 class SolarSystem {
     constructor(numPlanets) {
         this.planets = this.generatePlanets(numPlanets);
@@ -68,8 +63,7 @@ class SolarSystem {
     }
 }
 
-// Simulation du générateur de système solaire
-const mySolarSystem = new SolarSystem(5); // Crée un système avec 5 planètes
+const mySolarSystem = new SolarSystem(5);
 mySolarSystem.describe();
 
 console.log("\nPlanète la plus peuplée :");
