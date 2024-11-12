a93c05ef1735df0d852a850d3b7ac595769c3773
Jolann <jmadec@enseirb-matmeca.fr>
2024-11-12 15:06:33 +0100

    Init architecture

---
+++ crud/tp/crud/accountDAO.mjs
@@ -0,0 +1,8 @@
+import { ACCOUNT_LIST } from "./database.mjs";
+
+export const accountDAO = {
+  insertAccount(account) {},
+  retrieveAccountList() {},
+  updateAccount(account) {},
+  retrieveAccount(id) {},
+};
---
+++ crud/tp/crud/client.mjs
@@ -0,0 +1,1 @@
+import { accountService } from "./accountService.mjs";
